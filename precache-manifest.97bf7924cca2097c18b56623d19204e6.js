0f4afc127a9805bc82c2e40b9b6ee4fce79c5734
monirshimul <monirshimul@gmail.com>
2019-03-13 13:48:24 +0600

    Updates

---
+++ precache-manifest.97bf7924cca2097c18b56623d19204e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "e9a582893be627be3afa",
+    "url": "/contact-manager/static/js/main.e9a58289.chunk.js"
+  },
+  {
+    "revision": "273cd170738b9344b6fc",
+    "url": "/contact-manager/static/js/1.273cd170.chunk.js"
+  },
+  {
+    "revision": "e9a582893be627be3afa",
+    "url": "/contact-manager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "273cd170738b9344b6fc",
+    "url": "/contact-manager/static/css/1.55b3795a.chunk.css"
+  },
+  {
+    "revision": "e982b26bf7b353bac5cc74fa4850da04",
+    "url": "/contact-manager/index.html"
+  }
+];
